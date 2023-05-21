76acc9bbeff8a095267739316b4a20169b225c1a
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-05-21 11:04:59 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='genespectra',
+      version='0.1',
+      description='GENE classification and cross-SPECies comparison of cell Types with single cell RNA-seq data',
+      url='',
+      author='Yuyao Song',
+      author_email='ysong@ebi.ac.uk',
+      license='MIT',
+      packages=['genespectra'],
+      zip_safe=False)
