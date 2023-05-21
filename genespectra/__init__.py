c595600417e5aef75699e49c9cd5e882cf86bc56
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-05-21 11:30:10 +0100

    structure

--- genespectra/__init__.py
+++ genespectra/__init__.py
@@ -1,3 +1,37 @@
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+def greet(name):
+    return f"Hi {name}, you are gorgeous!"
+
+
+def make_metacells():
+
+
+
+def read_anndata():
+
+
+def check_anndata():
+
+
+def find_lowly_expressed_genes():
+
+
+def find_house_keeping_genes():
+
+
+def biomart_cell_cycle_genes():
+
+
+def fit_lda():
+
+
+
+def enrichment_go():
+
+
+def enrichment_kegg():
+
+
+
+def enrichment_tf():
+
+
