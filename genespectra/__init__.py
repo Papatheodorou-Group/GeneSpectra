366cd2969d171fb5dbfa6a7ec7b5af50f01ff2e3
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-05-23 22:38:55 +0100

    remote

--- genespectra/__init__.py
+++ genespectra/__init__.py
@@ -1,3 +1,14 @@
 def greet(name):
     return f"Hi {name}, you are gorgeous!"
 
+from anndata import AnnData, concat
+    from anndata import (
+        read_h5ad,
+        read_csv,
+        read_excel,
+        read_hdf,
+        read_loom,
+        read_mtx,
+        read_text,
+        read_umi_tools,
+    )
--- genespectra/metacells/make_metacells.py
+++ genespectra/metacells/make_metacells.py
@@ -1,11 +1,42 @@
-def make_metacells():
+import anndata as ad
+import matplotlib.pyplot as plt
+import metacells as mc
+import numpy as np
+import os
+import pandas as pd
+import scipy.sparse as sp
+import seaborn as sb
+import scanpy as sc
+
+from math import hypot
+from matplotlib.collections import LineCollection
+
+sb.set_style("white")
+
+
+def exclude_mt_genes(input_ad, col_use):
+    if col_use is not None and ~col_use in input_ad.var.columns:
+        raise KeyError(
+            f"{col_use} is not in {input_ad}.var.columns"
+        )
+    if col_use is None:
+        return input_ad[:, ~input_ad.var_names.str.startswith("MT-", "Mt-", "mt-")]
+    else:
+        return input_ad[:, ~input_ad.var[col_use].str.startswith("MT", "Mt-", "mt-")]
+
+
+def make_metacells(input_ad, target_umi, ):
+    """
+    Using metacell2 to create metacells from raw counts of scRNA-seq data
+
+    :return: an anndata object with each cell as a metacell
+    """
     print("make_metacells")
-
-
 
 
 def check_anndata():
     print("check_anndata")
+
 
 def summarize_counts():
     print("summarize_counts")
