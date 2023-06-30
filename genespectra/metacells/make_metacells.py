3a081aa00d2bb41de92b5359b968c2b2c0d95587
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-06-30 11:20:52 +0100

    cell pools and scripts

--- genespectra/metacells/make_metacells.py
+++ genespectra/metacells/make_metacells.py
@@ -371,3 +371,40 @@ def make_metacells_per_group(adata: anndata.AnnData,
 
     return final_mc_adata
 
+
+def sum_expression_by_class(adata, class_key):
+    """
+    Aggregate scRNA-seq data by cell class, create a "pseudo-bulk"
+    :param adata: input anndata object
+    :param class_key: the column in adata.obs indicating cell groups to combine
+    :return: an anndata object storing combined counts for each cell group
+    """
+    # Create a new AnnData object to store the summed expression levels
+    summed_adata = anndata.AnnData()
+
+    #  numerical class id does not work
+    adata.obs[class_key] = adata.obs[class_key].astype("str")
+
+    # Iterate over the unique classes in the given class key
+    unique_classes = adata.obs[class_key].unique()
+    for class_value in unique_classes:
+        # Subset the cells belonging to the current class
+        class_cells = adata[adata.obs[class_key] == class_value]
+
+        # Sum the expression levels across cells in the current class
+        summed_expression = class_cells.X.sum(axis=0)
+
+        # Create a temporary AnnData object for the current class
+        temp_adata = anndata.AnnData(X=summed_expression.reshape(1, -1))
+        temp_adata.obs[class_key] = class_value
+        temp_adata.obs_names = [class_value]
+
+        # Concatenate the temporary AnnData object with the summed_adata
+        if summed_adata.X is None:
+            summed_adata = temp_adata
+        else:
+            summed_adata = anndata.concat([summed_adata, temp_adata])
+
+    summed_adata.var = adata.var
+
+    return summed_adata
---
+++ genespectra/scripts/run_classification_sum_cell_pools.py
@@ -0,0 +1,26 @@
+import scanpy as sc
+import pandas as pd
+from genespectra.gene_classification.classify_genes import depth_normalize_counts, prepare_anndata_for_classification
+from genespectra.gene_classification.classify_genes import gene_classification_multiprocess
+from genespectra.metacells.make_metacells import sum_expression_by_class
+import click
+
+
+@click.command()
+@click.argument("input_h5ad", type=click.Path(exists=True))
+@click.argument("out_gene_class", type=click.Path(exists=False), default=None)
+@click.option('--anno_col', type=str, default=None, help="Cell class annotation column to use in input_h5ad")
+def run_classification_cell_pool(inpt_h5ad, out_gene_class, anno_col, **kwargs):
+    adata = sc.read_h5ad(inpt_h5ad)
+    summed_adata = sum_expression_by_class(adata, anno_col)
+
+    # when running on sum data, since different cell type have a different number of cells,
+    # normalize to a fixed size factor
+    ad_norm = depth_normalize_counts(summed_adata, target_sum=1000000)
+    data = prepare_anndata_for_classification(input_ad=ad_norm, anno_col=anno_col)
+    result_classes = gene_classification_multiprocess(data, **kwargs)
+    result_classes.to_csv(out_gene_class)
+
+
+if __name__ == '__main__':
+    run_classification_cell_pool()
