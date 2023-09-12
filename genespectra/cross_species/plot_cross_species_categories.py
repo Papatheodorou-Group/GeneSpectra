2dbf6447267992a1ccc0a272fa9a72441bd1ad5d
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-09-12 23:52:54 +0100

    add typings to plotting funcions

--- genespectra/cross_species/plot_cross_species_categories.py
+++ genespectra/cross_species/plot_cross_species_categories.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from plotly.graph_objs._imshow import Imshow
 
 
-def plot_cross_species_category_heatmap(mapping_both, species_1, species_2):
+def plot_cross_species_spec_category_heatmap(mapping_both, species_1, species_2, group_enhanced=False) -> Imshow:
+    if not group_enhanced:
+        mapping_both.loc[
+            mapping_both[f"spec_category_{species_1}"] == 'group enhanced', [f"spec_category_{species_1}"]] = 'enhanced'
+        mapping_both.loc[mapping_both[f"spec_category_{species_1}"] == 'cell type enhanced', [
+            f"spec_category_{species_1}"]] = 'enhanced'
+
+        mapping_both.loc[
+            mapping_both[f"spec_category_{species_2}"] == 'group enhanced', [f"spec_category_{species_2}"]] = 'enhanced'
+        mapping_both.loc[mapping_both[f"spec_category_{species_2}"] == 'cell type enhanced', [
+            f"spec_category_{species_2}"]] = 'enhanced'
 
     df = mapping_both.loc[mapping_both[f"{species_2}_homolog_orthology_type"] == 'ortholog_one2one'].groupby(
-        [f"spec_category_{species_1}"])[f"spec_category_{species_2}"]\
+        [f"spec_category_{species_1}"])[f"spec_category_{species_2}"] \
         .value_counts().to_frame().reset_index()
 
     species1_counts = df.groupby(f"spec_category_{species_1}")['count'].sum()
@@ -28,15 +39,22 @@ def plot_cross_species_category_heatmap(mapping_both, species_1, species_2):
 
     heatmap_data = np.round(heatmap_data.astype("float32"), 4)
 
-    order = ['cell type enriched',
-             'group enriched',
-             'cell type enhanced',
-             'group enhanced',
-             'low cell type specificity',
-             'lowly expressed']
+    if not group_enhanced:
 
-    fig = px.imshow(heatmap_data[order].reindex(order),
-                    title='One2one orthologs',
+        order = ['cell type enriched',
+                 'group enriched',
+                 'enhanced',
+                 'low cell type specificity',
+                 'lowly expressed']
+    else:
+        order = ['cell type enriched',
+                 'group enriched',
+                 'cell type enhanced',
+                 'group enhanced',
+                 'low cell type specificity',
+                 'lowly expressed']
+
+    fig = px.imshow(heatmap_data[order].reindex(order), title='One2one orthologs',
                     text_auto=True,
                     width=750, height=600,
                     labels=dict(x=species_2, y=species_1, color="% overlap"))
@@ -46,3 +64,44 @@ def plot_cross_species_category_heatmap(mapping_both, species_1, species_2):
 
     return fig
 
+
+def plot_cross_species_dist_category_heatmap(mapping_both, species_1, species_2) -> Imshow:
+    df = mapping_both.loc[mapping_both[f"{species_2}_homolog_orthology_type"] == 'ortholog_one2one'].groupby(
+        [f"dist_category_{species_1}"])[f"dist_category_{species_2}"] \
+        .value_counts().to_frame().reset_index()
+
+    species1_counts = df.groupby(f"dist_category_{species_1}")['count'].sum()
+    species2_counts = df.groupby(f"dist_category_{species_2}")['count'].sum()
+
+    df['Species1_Percentage'] = df.apply(
+        lambda row:
+        (row['count'] / species1_counts[row[f"dist_category_{species_1}"]]) * 100,
+        axis=1)
+    df['Species2_Percentage'] = df.apply(
+        lambda row:
+        (row['count'] / species2_counts[row[f"dist_category_{species_2}"]]) * 100,
+        axis=1)
+    df['Harmonic_Mean'] = 2 / ((1 / df['Species1_Percentage']) + (1 / df['Species2_Percentage']))
+
+    heatmap_data = df.pivot(columns=f"dist_category_{species_1}",
+                            index=f"dist_category_{species_2}",
+                            values='Harmonic_Mean')
+
+    heatmap_data = np.round(heatmap_data.astype("float32"), 4)
+
+    order = ['expressed in over 90%',
+             'expressed in over 50%',
+             'expressed in over 25%',
+             'expressed in less than 25%',
+             'expressed in single',
+             'lowly expressed']
+
+    fig = px.imshow(heatmap_data[order].reindex(order), title='One2one orthologs',
+                    text_auto=True,
+                    width=750, height=600,
+                    labels=dict(x=species_2, y=species_1, color="% overlap"))
+    fig['layout']['yaxis']['autorange'] = "reversed"
+    fig.update_yaxes(autorange=True)
+    fig.show()
+
+    return fig
--- genespectra/gene_classification/plotting.py
+++ genespectra/gene_classification/plotting.py
@@ -1,11 +1,13 @@
 import plotly.express as px
+from plotly.graph_objs._imshow import Imshow
 import matplotlib.pyplot as plt
+from matplotlib.axes._axes import Axes
 import seaborn as sns
 sns.set_theme(rc={'figure.dpi': 100, 'figure.figsize': (2, 2)})
-import pandas as pd
+import numpy as np
 
 
-def plot_mean_var(adata, mean_ref, var_ref):
+def plot_mean_var(adata, mean_ref, var_ref) -> Axes:
     ax = plt.axes()
     sns.scatterplot(data=adata.var, x="gene_mean_log1psf", y="gene_var_log1psf", size=1)
     if mean_ref is not None:
@@ -15,7 +17,7 @@ def plot_mean_var(adata, mean_ref, var_ref):
     return ax
 
 
-def plot_mean_var_pie(adata):
+def plot_mean_var_pie(adata) -> Axes:
     if 'low_variance' not in adata.var.columns or 'low_mean' not in adata.var.columns:
         raise KeyError("low variance and low mean not in adata.var.columns"
                        "run find_low_variance_genes and find_low_expression_genes on adata")
@@ -37,7 +39,7 @@ def plot_mean_var_pie(adata):
     return ax
 
 
-def plot_categories_pie(data):
+def plot_categories_pie(data) -> Imshow:
     """
 
     :param data: the categories output by hpa_gene_classification
@@ -51,7 +53,7 @@ def plot_categories_pie(data):
     return fig
 
 
-def plot_categories_hist(data):
+def plot_categories_hist(data) -> Imshow:
     fig = px.histogram(data, x='n_exp', color='spec_category', nbins=30,
                        opacity=0.8, barmode='stack')
 
