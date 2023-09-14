b7dbce79e12369bc5342c730b8c8eb8c1ff4519c
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2023-09-14 17:07:43 +0100

    debug

--- genespectra/main.py
+++ genespectra/main.py
@@ -5,14 +5,9 @@ import os
 directory_of_this_script = os.path.dirname(os.path.abspath(__file__))
 print(directory_of_this_script)
 
-from genespectra.gene_classification.classify_genes import remove_not_profiled_genes
-from genespectra.gene_classification.classify_genes import remove_cell_cycle_genes
-from genespectra.gene_classification.classify_genes import remove_low_counts_genes
-from genespectra.gene_classification.classify_genes import depth_normalize_counts
-from genespectra.gene_classification.classify_genes import find_low_count_genes
-from genespectra.gene_classification.classify_genes import hpa_gene_classification
+from genespectra.gene_classification.classify_genes import *
 
-
+"""
 
 ad = sc.read_h5ad("/nfs/research/irene/ysong/RESULTS/GeneCellType/SCCAF_clustering_opt/Hearts/metacell_topics"
                   "/hs_heart_metacells_anno.h5ad")
@@ -23,3 +18,4 @@ ad = remove_low_counts_genes(ad)
 ad = remove_cell_cycle_genes(ad)
 
 result = hpa_gene_classification(ad, anno_col='cell_ontology_base', max_group_n=None, exp_lim=1, enr_fold=4)
+"""
