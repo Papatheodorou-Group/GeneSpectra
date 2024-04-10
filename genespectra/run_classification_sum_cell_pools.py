7a58090a6e6fc5b217ee4b7aacab047c1bf5a173
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2024-04-10 09:20:41 +0200

    add script

--- genespectra/run_classification_sum_cell_pools.py
+++ genespectra/run_classification_sum_cell_pools.py
@@ -1,6 +1,6 @@
 import scanpy as sc
 from genespectra.gene_classification.classify_genes import ExpressionDataLong, GeneClassificationResult
-from genespectra.metacells.make_metacells import SummedAnnData
+from genespectra.metacells.make_metacells import SummedAnnData, sum_expression_by_class
 import click
 
 
@@ -9,12 +9,19 @@ import click
 @click.argument("out_gene_class", type=click.Path(exists=False), default=None)
 @click.option('--anno_col', type=str, default=None, help="Cell class annotation column to use in input_h5ad")
 def run_classification_cell_pool(input_h5ad, out_gene_class, anno_col, **kwargs):
-    adata = sc.read_h5ad(input_h5ad)
-    summed_adata = SummedAnnData.create_from_anndata(adata, anno_col)
+    adata_sc = sc.read_h5ad(input_h5ad)
+
+    # simply make pseudobulks
+    adata = sum_expression_by_class(adata_sc, annotation_col=anno_col)
+    # average expression by group, which doesn't do anything if the data is summed pseudobulk
+    summed_adata = SummedAnnData.create_from_summed_adata(adata, anno_col=anno_col)
+    print(summed_adata)
+
     # normalize to a fixed size factor
     summed_adata = SummedAnnData.depth_normalize_counts(summed_adata, target_sum=1000000)
     sc.pp.calculate_qc_metrics(summed_adata, log1p=False, inplace=True)
     summed_adata = SummedAnnData.filter_low_counts(summed_adata, min_count=1)
+
     print(f"running gene classification on {len(summed_adata.var_names.values)} genes")
     expr_data = ExpressionDataLong.create_from_summed_adata(input_summed_adata=summed_adata, anno_col=anno_col)
     result_classes = GeneClassificationResult.create_from_expression_data_long_multiprocess(expr_data, **kwargs)
