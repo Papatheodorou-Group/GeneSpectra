ce7abdf8e8fbae22313684cfd6308617581bf6b4
YY-SONG0718 <794815059@qq.com>
2024-12-15 13:59:49 +0000

    switch to pixi for dep management

--- genespectra/__init__.py
+++ genespectra/__init__.py
@@ -2,3 +2,7 @@ __all__ = ["gene_classification", "metacells", "cross_species"]
 from .gene_classification import *
 from .metacells import *
 from .cross_species import *
+
+
+def say_hello():
+    return print("hello")
